9457e4d0bdd842b8cce4177c9acfd9269a0bff68
Joseph Kiehna <jhkiehna@gmail.com>
2019-05-19 21:13:05 -0400

    working on Top component skeleton

--- src/components/Top.js
+++ src/components/Top.js
@@ -1,9 +1,38 @@
 import React from "react";
+import { getTopStories } from "../utils/api";
 
 export default class Top extends React.Component {
-  state = {};
+  state = {
+    stories: []
+  };
+
+  componentDidMount() {
+    this.setState({ stories: getTopStories() });
+  }
 
   render() {
-    return <h1>Top News Component</h1>;
+    console.log(this.state);
+
+    return (
+      <>
+        <h1>Top News Component</h1>
+
+        <ul>
+          {this.state.stories.map((story, index) => {
+            const { title, author, date } = story;
+
+            return (
+              <li key={index}>
+                <ul>
+                  <li>{title}</li>
+                  <li>{author}</li>
+                  <li>{date}</li>
+                </ul>
+              </li>
+            );
+          })}
+        </ul>
+      </>
+    );
   }
 }
---
+++ src/utils/api.js
@@ -0,0 +1,16 @@
+const MOCK_STORIES = [
+  {
+    title: "FAke story 1",
+    author: "so and so",
+    date: "01/01/1970"
+  },
+  {
+    title: "FAke story 2",
+    author: "another person",
+    date: "01/01/1971"
+  }
+];
+
+export function getTopStories() {
+  return MOCK_STORIES;
+}
